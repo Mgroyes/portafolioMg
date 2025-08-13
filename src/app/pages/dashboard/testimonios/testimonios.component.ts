8ad68dfdeac0eef8597005bf2f9ab53e0ff4e0e1
Miguel <mgroyes22@gmail.com>
2025-08-13 11:10:09 -0500

    ultimos cambios

--- src/app/pages/dashboard/testimonios/testimonios.component.ts
+++ src/app/pages/dashboard/testimonios/testimonios.component.ts
@@ -28,7 +28,14 @@ export class TestimoniosComponent {
       role: 'Product Manager / Triidy',
       avatar: 'assets/imgs/avatar2.png',
       text: `Sus diseños y procesos convierten lo funcional en productos agradables para los usuarios, pasando de lo complejo a lo intuitivo, colaborando siempre con la mejor actitud ante los retos que se afrontan.`
+    },
+     {
+      name: 'Mateo Lopez',
+      role: 'Diseñador 3D / INJ Portugal',
+      avatar: 'assets/imgs/avatar-3.png',
+      text: `A lo largo de más de 10 años hemos participado juntos en varios proyectos, siempre dentro de un proceso creativo constante. Trabajar juntos ha sido muy enriquecedor: su trabajo dedicado, organizado y con una gran capacidad para trabajar en equipo. Su compromiso y su forma meticulosa de abordar cada tarea hacen que el trabajo fluya y que los resultados siempre superen las expectativas. Además, su disposición para aportar ideas y colaborar hace que cada proyecto sea no solo productivo, sino también inspirador.`
     }
+    
   ];
 
   currentIndex = 0;
